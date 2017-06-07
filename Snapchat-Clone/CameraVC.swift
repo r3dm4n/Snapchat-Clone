2a6ab2b4549ee6375b408acc86fd220cf7aac2a0
Alexandru Corut <r3dm4n@me.com>
2017-06-07 16:55:16 +0300

    protocol & delegate

--- Snapchat-Clone/CameraVC.swift
+++ Snapchat-Clone/CameraVC.swift
@@ -8,19 +8,64 @@
 
 import UIKit
 
-class CameraVC: AAPLCameraViewController {
+class CameraVC: AAPLCameraViewController, AAPLCameraVCDelegate {
 
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var cameraBtn: UIButton!
     @IBOutlet weak var previewView: AAPLPreviewView!
     
     override func viewDidLoad() {
-    self._previewView = previewView
-        super.viewDidLoad()
+
+
+        delegate = self
+        self._previewView = previewView
+    super.viewDidLoad()
 
     
     }
 
- 
+    @IBAction func changeCameraActionPressed(_ sender: Any) {
+        changeCamera()
+    }
+    
+    @IBAction func recordBtnPressed(_ sender: Any) {
+        toggleMovieRecording()
+        
+    }
+    
+    func shouldEnableCameraUI(_ enable: Bool) {
+        cameraBtn.isEnabled = enable
+        print("Should enable record UI:\(enable)")
+    }
+    
+    func shouldEnableRecordUI(_ enable: Bool) {
+        recordBtn.isEnabled = enable
+        print("Should enable camera UI: \(enable)")
+    }
 
+    func recordingHasStarted() {
+        print("Recording has started")
+    }
+    
+    func canStartRecording() {
+        print("Can start recording")
+    }
+    
+    func videoRecordingComplete(_ videoURL: URL!) {
+        print("Video recording complete")
+    }
+    
+    func videoRecordingFailed() {
+        print("Video recording failed")
+    }
+    
+    func snapshotTaken(_ snapshotData: Data!) {
+        print("Snapshot taken")
+    }
+    
+    func snapshotFailed() {
+        print("Snapshot failed")
+    }
 }
 
     
